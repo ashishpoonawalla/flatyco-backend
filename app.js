6975c006daa07a432118b42d7526a23b08d19901
Ashish Poonawalla <ashish.omar.pune@gmail.com>
2023-08-11 10:45:13 +0530

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const exprss = require('express')
 const app = express()
 
 app.get('/', (req,res)=>{
-    res.json({mesg:'Hello Backend'})
+    res.json({mesg:'Hello Backend!'})
 })
 
 app.listen(3000)
